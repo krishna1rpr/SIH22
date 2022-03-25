4e899d5c98b10ddd8033bd1fdfdcc7f5f2c52cee
Yash Joglekar <yashjoglekar1220@gmail.com>
2022-03-25 11:58:27 +0530

    Folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,31 @@ import { Switch, Route } from "react-router-dom";
 import "./static/scss/app.scss";
 import { Spinner } from "react-bootstrap";
 import NavBar from "./components/NavBar";
+import Map from "./pages/map";
 const Hero = lazy(() => import("./components/Hero"));
 
 const App = () => {
-  return (
-    <>
-      <NavBar />
-      <Suspense
-        fallback={
-          <Spinner animation="border" role="status">
-            <span className="visually-hidden">Loading...</span>
-          </Spinner>
-        }
-      >
-        <Hero />
-        <Switch>
-          <Route path="/"></Route>
-        </Switch>
-      </Suspense>
-    </>
-  );
+    return (
+        <>
+            <NavBar />
+            <Suspense
+                fallback={
+                    <Spinner animation="border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </Spinner>
+                }
+            >
+                <Switch>
+                    <Route exact path="/">
+                        <Hero />
+                    </Route>
+                    <Route exact path="/map">
+                        <Map />
+                    </Route>
+                </Switch>
+            </Suspense>
+        </>
+    );
 };
 
 export default App;
