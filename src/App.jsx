c90994eaceba6707f0710de959a8e017f49f4281
nit <nitish.headfirst@outlook.com>
2022-03-25 12:25:24 +0530

    remove unused import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense } from "react";
+import { Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
 import "./static/scss/app.scss";
 import { Spinner } from "react-bootstrap";
@@ -7,36 +7,35 @@ import Map from "./pages/map";
 import CreateAlert from "./pages/createAlert";
 import About from "./pages/about";
 import Home from "./pages/home";
-const Hero = lazy(() => import("./components/Hero"));
 
 const App = () => {
-    return (
-        <>
-            <NavBar />
-            <Suspense
-                fallback={
-                    <Spinner animation="border" role="status">
-                        <span className="visually-hidden">Loading...</span>
-                    </Spinner>
-                }
-            >
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route exact path="/map">
-                        <Map />
-                    </Route>
-                    <Route exact path="/about">
-                        <About />
-                    </Route>
-                    <Route exact path="/create-alert">
-                        <CreateAlert />
-                    </Route>
-                </Switch>
-            </Suspense>
-        </>
-    );
+  return (
+    <>
+      <NavBar />
+      <Suspense
+        fallback={
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        }
+      >
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/map">
+            <Map />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/create-alert">
+            <CreateAlert />
+          </Route>
+        </Switch>
+      </Suspense>
+    </>
+  );
 };
 
 export default App;
